5dadce4d9b082baf5741ce1f533d33d36be5843d
htmlka228 <55635809+htmlka228@users.noreply.github.com>
2021-06-15 12:54:31 +0400

    Delete product-service directory

--- product-service/src/main/java/com/example/productservice/entity/ProductAvailable.java
+++
@@ -1,9 +0,0 @@
-package com.example.productservice.entity;
-
-import lombok.Data;
-
-@Data
-public class ProductAvailable {
-    private String uniq_id;
-    private Integer availableQuantity = 0;
-}
